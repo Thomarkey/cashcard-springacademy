cb4b886d356f1a42461c54666c35fdb40c00b389
Thomarkey <thoma_cz@hotmail.com>
2024-10-09 10:40:07 +0200

    added DELETE endpoint + testcases

--- src/main/java/example/cashcard/SecurityConfig.java
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -46,7 +46,13 @@ public class SecurityConfig {
                 .roles("NON-OWNER")
                 .build();
 
-        return new InMemoryUserDetailsManager(sarah, hanksOwnsNoCards);
+        UserDetails kumar = users
+                .username("kumar2")
+                .password(passwordEncoder.encode("xyz789"))
+                .roles("CARD-OWNER")
+                .build();
+
+        return new InMemoryUserDetailsManager(sarah, hanksOwnsNoCards, kumar);
     }
 
 }
